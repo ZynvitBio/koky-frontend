52b8d69b33bf202aab88b1e77e683a3f7d454081
jonathan <jonathanbb.com@gmail.com>
2026-07-23 19:20:18 -0500

    chore: update googleMapsApiKey to the new production key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  googleMapsApiKey: 'AIzaSyAGbY15elfHQR1Q8AmTQ9A7Tt_lo5ZvLPs',
+  googleMapsApiKey: 'AIzaSyCgNePvHoUvXXjMj-Ocbkp4WVvyt87du2o',
   apiUrl: 'https://koky-backend-production.up.railway.app',
   wompiPublicKey: 'pub_prod_fiQqn8TiiKqVLDEsmrL5LD2LSiau8xmz',
   wompiIntegrityKey: '' // Pegar la Llave de integridad aquí
